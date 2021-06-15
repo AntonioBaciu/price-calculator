66f861afe70a8d8cc98c6ebc8dbfdae091234058
Prince Issar <82021741+PrinceIssar@users.noreply.github.com>
2021-06-15 11:27:27 +0200

    add customer class

---
+++ Model/Customer.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+class Customer
+{
+    private int $id;
+    private string $firstName;
+    private string $lastName;
+    private int $fixDiscount;
+    private int $varDiscount;
+    private Group $group;
+
+
+    public function __construct(int $id, string $firstName, string $lastName, int $fixDiscount, int $varDiscount, Group $group)
+    {
+        $this->id = $id;
+        $this->firstName = $firstName;
+        $this->lastName = $lastName;
+        $this->fixDiscount = $fixDiscount;
+        $this->varDiscount = $varDiscount;
+        $this->group = $group;
+    }
+
+    public function getId():int {
+        return $this->id;
+    }
+    public function getFirstName(): string
+    {
+        return $this->firstName;
+    }
+
+    public function getLastName(): string
+    {
+        return $this->lastName;
+    }
+
+    public function getFixDiscount(): int
+    {
+        return $this->fixDiscount;
+    }
+
+    public function getVarDiscount(): int
+    {
+        return $this->varDiscount;
+    }
+
+    public function getGroup(): Group
+    {
+        return $this->group;
+    }
+}
