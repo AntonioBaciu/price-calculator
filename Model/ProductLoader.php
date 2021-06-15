f895f3c9eddbd798d3f5b1fb655b5c14613b968c
Antonio Baciu <antoniopaulobaciu@gmail.com>
2021-06-15 16:44:23 +0200

    Uploading Product Loader

---
+++ Model/ProductLoader.php
@@ -0,0 +1,22 @@
+<?php
+
+class ProductLoader extends Connection
+{
+    private array $products;
+
+
+    public function loadProduct() :void {
+        $pdo = $this->openConnection();
+        $statement = $pdo->prepare('SELECT * FROM product');
+        $statement->execute();
+        $products = $statement->fetchAll();
+        foreach ($products as $product) {
+            $this->products[$product['id']] = new Product((int)$product['id'], (string)$product['name'], (string)$product['price']);
+        }
+    }
+
+    public function getProducts(): array {
+        $this->loadProduct();
+        return $this->products;
+    }
+}
